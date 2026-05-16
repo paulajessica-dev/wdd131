d9082bdae93da68ea4a64c4614d90547380830ca
paulajessica-dev <jessicafldasilva@gmail.com>
2026-05-16 09:50:26 -0300

    add week02 assign

--- week02_assignment/scripts/temples.js
+++ week02_assignment/scripts/temples.js
@@ -5,3 +5,11 @@ year.textContent = new Date().getFullYear();
 
 lastModified.textContent =
     `Last Modification: ${document.lastModified}`;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
