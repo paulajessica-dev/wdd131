47f00c89aaf5eef400b012acc9151e483119839b
paulajessica-dev <jessicafldasilva@gmail.com>
2026-05-30 17:51:43 -0300

    add activity week04

--- week04_assignment/scripts/temple.js
+++ week04_assignment/scripts/temple.js
@@ -6,6 +6,13 @@ year.textContent = new Date().getFullYear();
 lastModified.textContent =
     `Last Modification: ${document.lastModified}`;
 
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
 
 const temples = [
   {
@@ -67,25 +74,100 @@ const temples = [
   {
     templeName: "Fortaleza City Brazil",
     location: "Fortaleza City, Brazil",
-    dedicated: "",
-    area: ,
+    dedicated: "2019, June, 2",
+    area: 9295,
     imageUrl:
-    ""
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/fortaleza-brazil/800x500/5-Fortaleza-Temple-Photo-2219775.jpg"
   },
   {
-    templeName: "Fortaleza City Brazil",
-    location: "Fortaleza City, Brazil",
-    dedicated: "",
-    area: ,
+    templeName: "são Paulo City Brazil",
+    location: "São Paulo City, Brazil",
+    dedicated: "1978, October, 30",
+    area: 5110,
     imageUrl:
-    ""
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/sao-paulo-brazil/800x500/sao-paulo-brazil-temple-lds-187030-wallpaper.jpg"
   },
   {
-    templeName: "Fortaleza City Brazil",
-    location: "Fortaleza City, Brazil",
-    dedicated: "",
-    area: ,
+    templeName: "Salt Lake City United States",
+    location: "Salt Lake City, United States",
+    dedicated: "1893, April, 6",
+    area: 35508,
     imageUrl:
-    ""
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/salt-lake-city-utah/800x500/salt-lake-temple-37762.jpg"
   }
 ];
+
+
+createCard(temples);
+
+const oldTempleLink = document.querySelector("#old");
+
+oldTempleLink.addEventListener("click", () => {
+      event.preventDefault();
+      createCard(temples.filter(temple => {
+      const year = parseInt(temple.dedicated.split(",")[0]);
+      return year < 1900;
+    }));
+
+});
+
+const newTempleLink = document.querySelector("#new");
+
+newTempleLink.addEventListener("click", () => {
+      event.preventDefault();
+      createCard(temples.filter(temple => {
+      const year = parseInt(temple.dedicated.split(",")[0]);
+      return year > 2000;
+    }));
+
+});
+
+const largeTempleLink = document.querySelector("#large");
+
+largeTempleLink.addEventListener("click", () => {
+      event.preventDefault();
+      createCard(temples.filter(temple => {
+      const area = temple.area;
+      return area >= 90000;
+    }));
+
+});
+
+const smallTempleLink = document.querySelector("#small");
+
+smallTempleLink.addEventListener("click", () => {
+      event.preventDefault();
+      createCard(temples.filter(temple => {
+      const area = temple.area;
+      return area <= 10000;
+    }));
+
+});
+
+const homeTempleLink = document.querySelector("#home");
+
+homeTempleLink.addEventListener("click", () => {
+      event.preventDefault();
+      createCard(temples);
+
+});
+
+function createCard(templesList){
+    document.querySelector("#gallery").innerHTML = "";
+    const gallery = document.querySelector("#gallery");
+    console.log(gallery);
+    templesList.forEach(temple => {
+      const card = document.createElement("section");
+      card.classList.add("card");
+      card.innerHTML = `
+            <h3>${temple.templeName}</h3>
+            <p><strong>Location:</strong> ${temple.location}</p>
+            <p><strong>Dedicated:</strong> ${temple.dedicated}</p>
+            <p><strong>Size:</strong> ${temple.area} sq ft</p>
+            <img src="${temple.imageUrl}" alt="${temple.templeName}" loading="lazy">
+        `;
+
+        gallery.appendChild(card);
+    });
+}
+
